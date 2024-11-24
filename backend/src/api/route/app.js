68c4ee604c5d7e25562d4f45e625b725e1048f15
JibAb96 <j.abshir96@gmail.com>
2024-11-24 17:03:34 +0100

    Create file for route folder containing all the routes for the application.

--- backend/src/api/route/app.js
+++ backend/src/api/route/app.js
@@ -1,16 +1,14 @@
 import express from "express";
+import createPost from "../controllers/createPost.js";
+import getPosts from "../controllers/getPosts.js";
+import updatePost from "../controllers/updatePost.js";
+import deletePost from "../controllers/deletePost.js";
 
 const router = express.Router();
 
-router.get("/", 
-    //getPosts
-    );
-router.post("/create",
-    //createPost
-);
-router.put("/edit",
-    //editPost
-);
-router.delete("/", 
-    //deletePost
-);
+router.get("/", (req, res) => getPosts(req, res));
+router.post("/", (req, res) => createPost(req, res));
+router.put("/", (req, res) => updatePost(req, res));
+router.delete("/:id", (req, res) => deletePost(req, res));
+
+export default router;
