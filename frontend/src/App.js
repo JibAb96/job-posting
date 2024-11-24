7f1df6c5b161a938e3a472d93b5de80694dd6189
JibAb96 <j.abshir96@gmail.com>
2024-11-24 17:06:06 +0100

    Add components that will be rendered on page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import The_Navbar from './components/Navbar/The_Navbar';
+import Hero from './components/Hero/Hero';
+import JobPostingSection from './components/JobPostingSection/JobPostingSection';
+import { ShowModalProvider } from './context/ShowModal';
+import CreateJobPostModal from './components/CreateJobPostModal/CreateJobPostModal';
 const App = () => {
-
-  
+  return(
+    <ShowModalProvider>
+      <The_Navbar/>
+      <Hero/>
+      <JobPostingSection/>
+      <CreateJobPostModal/>
+    </ShowModalProvider>
+  )
 }
 
 export default App;
