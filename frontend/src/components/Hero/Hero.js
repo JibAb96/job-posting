4dc8da93d92d5cb98d74f96288dfc08c1279ecc6
JibAb96 <j.abshir96@gmail.com>
2024-11-24 17:07:49 +0100

    Create Hero component for aesthetics purposes.

---
+++ frontend/src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1>The Easiest Way To Get Your Dream Job</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
