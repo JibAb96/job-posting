6500d792ec6c3ed2eeae76ffaf967488b5b457cf
JibAb96 <j.abshir96@gmail.com>
2024-11-24 17:02:00 +0100

    Create model forconnecting to the database

---
+++ backend/src/api/model/db.js
@@ -0,0 +1,17 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+});
+
+export default pool
+
